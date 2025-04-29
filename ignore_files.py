5c56a59a27818022b36ee47c0f50954d748444a7
Jerald Achaibar <jeraldthatman@gmail.com>
2025-04-28 23:11:08 -0400

    cleanup

---
+++ ignore_files.py
@@ -0,0 +1,37 @@
+import os 
+import shutil
+
+#### Do NOT DELETE ANY FILES ####
+
+
+# Ignore filetypes
+ignore_filetypes = ['.pyc','ipynb','pkl']
+
+ignore_folders = ['__pycache__', '.git']
+# List all files in the current directory
+files = os.listdir('.')
+print("Files in the current directory:")
+
+cases = []
+
+for folder in files:
+    if folder in ignore_folders:
+        continue
+    # Check if the folder is a directory
+    if os.path.isdir(folder):
+        print(f"Directory: {folder}")
+        for root, dirs, files in os.walk(folder):
+            for filename in files:
+                if not any(filename.endswith(ext) for ext in ignore_filetypes):
+                    print(f"\tFile: {filename}")
+                else:
+                    cases.append(os.path.join(root, filename))
+    else:
+        print(f"File: {folder}")
+
+print("Files to be ignored:")
+sorted(cases)
+with open('.gitignore', 'w') as f:
+    for case in cases:
+        f.write(case + '\n')
+        print(case)
